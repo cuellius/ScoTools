99841443b2e58e234502490500bb49f1739aaaeb
Jan <31709392+cuellius@users.noreply.github.com>
2018-06-24 18:55:15 +0300

    Add files via upload

---
+++ ScoUtils/ScoReader.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <cstdio>
+#include "Sco.h"
+
+void read_sco_file(FILE *file, sco_file_t *dest);
---
+++ ScoUtils/ScoWriter.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <cstdio>
+#include "Sco.h"
+
+void write_sco_file(FILE *file, sco_file_t *src);
